0fcb4f3ad9417d42bc03f4de256aad773b331413
Keith <56834121+TheRealKeith@users.noreply.github.com>
2020-11-27 13:52:44 -0700

    C# Export Fix (#1)

--- Application/Elements/TextEntryElement.cs
+++ Application/Elements/TextEntryElement.cs
@@ -72,6 +72,7 @@ namespace GumpStudio.Elements
 		{
 			_Size = new Size(200, 20);
 			mHue = Hues.GetHue(0);
+			mInitialText = string.Empty;
 		}
 
 		public TextEntryElement(SerializationInfo info, StreamingContext context) : base(info, context)
@@ -142,11 +143,9 @@ namespace GumpStudio.Elements
 		public string ToCSharpString()
 		{
 			if (MaxLength > 0)
-			{
-				return $"AddTextEntry({X}, {Y}, {Width}, {Height}, {Hue}, (int)Inputs.{Name.Replace(" ", String.Empty)}, {InitialText.Replace("\"", "\\\"")}, {MaxLength});";
-			}
-
-			return $"AddTextEntry({X}, {Y}, {Width}, {Height}, {Hue}, (int)Inputs.{Name.Replace(" ", String.Empty)}, {InitialText.Replace("\"", "\\\"")});";
+				return $"AddTextEntry({X}, {Y}, {Width}, {Height}, {Hue}, (int)Inputs.{Name?.Replace(" ", string.Empty)}, \"{InitialText?.Replace("\"", "\\\"")}\", {MaxLength});";
+				
+			return $"AddTextEntry({X}, {Y}, {Width}, {Height}, {Hue}, (int)Inputs.{Name?.Replace(" ", string.Empty)}, \"{InitialText?.Replace("\"", "\\\"")}\");";
 		}
 	}
 }
